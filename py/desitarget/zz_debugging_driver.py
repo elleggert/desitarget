816b3c10d91f67ddc9fe3cda1e7bee0fa45760b0
ele20 <ele20@ic.ac.uk>
2021-12-31 16:22:14 +0100

    Started with a driver to debug

---
+++ py/desitarget/zz_debugging_driver.py
@@ -0,0 +1,21 @@
+import os
+from cuts import select_targets
+import pandas as pd
+
+filenames = []
+
+path = '../../../bricks_data/tractor/'
+
+for filename in os.listdir(path):
+    if '.fits' not in filename:
+        continue
+    filenames.append(f'{path}/{filename}')
+
+
+res = select_targets(
+    infiles=filenames, numproc=1, qso_selection='colorcuts', nside=None, gaiasub=False,
+    tcnames=['LRG', 'ELG', 'QSO'], backup=False)
+
+
+
+df = pd.DataFrame(data=res)
