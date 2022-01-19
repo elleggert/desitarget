8a406eaba1e73933e8150d0881f962710b49d8b4
ele20 <ele20@ic.ac.uk>
2022-01-19 18:36:30 +0100

    Separated out format results function into cuts

--- py/desitarget/cuts.py
+++ py/desitarget/cuts.py
@@ -41,6 +41,8 @@ from desitarget.geomask import bundle_bricks, pixarea2nside, sweep_files_touch_h
 from desitarget.geomask import box_area, hp_in_box, is_in_box, is_in_hp
 from desitarget.geomask import cap_area, hp_in_cap, is_in_cap, imaging_mask
 
+import pandas as pd # ADDED BY EE TO RETURN CLASSIFIED DATA IN CSV FORMAT
+
 # ADM set up the DESI default logger
 from desiutil.log import get_logger
 log = get_logger()
@@ -2317,6 +2319,8 @@ def set_target_bits(photsys_north, photsys_south, obs_rflux,
                     maskbits, Grr, refcat, primary, resolvetargs=True):
     """Perform target selection on parameters, return target mask arrays.
 
+    NOTE: FUNCTION MODIFIED BY EE TO EXTRACT LYMAN BREAK GALAXIES
+
     Parameters
     ----------
     photsys_north, photsys_south : :class:`~numpy.ndarray`
@@ -2724,7 +2728,8 @@ def isLBG(gnobs, gsnr, maskbits, photsys_north, photsys_south, primary, rnobs, r
         ----------
         - See :func:`~desitarget.cuts.set_target_bits` for other parameters.
 
-
+        NOTE: FUNCTION ADDED BY EE
+    
         Returns
         -------
         :tuple of class:`array_like`
@@ -2745,6 +2750,87 @@ def isLBG(gnobs, gsnr, maskbits, photsys_north, photsys_south, primary, rnobs, r
     rlbg = (rdropout_north & photsys_north) | (rdropout_south & photsys_south)
 
     return glbg, rlbg
+
+
+def desitarget_bitcode_2_str(bitcode):
+    bina = (bin(bitcode))
+    bina = bina[2:]
+
+    desi_codes = set()
+
+    i = len(bina) - 1
+    for bit in bina:
+        if bit == '1':
+            desi_codes.add(i)
+        i -= 1
+
+    return desi_codes
+
+
+def desi_code_2_label(code):
+
+    LRG_CODES = {0, 8, 16}
+    ELG_CODES = {1, 5, 6, 7, 9, 11, 12, 17, 19, 20}
+    QSO_CODES = {2, 4, 10, 18}
+    GLBG_CODE = {38}
+    RLBG_CODE = {39}
+
+    lrg = bool(LRG_CODES & code)
+
+    elg = bool(ELG_CODES & code)
+
+    qso = bool(QSO_CODES & code)
+
+    glbg = bool(GLBG_CODE & code)
+
+    rlbg = bool(RLBG_CODE & code)
+
+    return lrg, elg, qso, glbg, rlbg
+
+
+def desi_code_2_cat(bitcode):
+    return desi_code_2_label(desitarget_bitcode_2_str(bitcode))
+
+
+def format_output(result):
+    cols = [('RELEASE', '>i2'), ('BRICKID', '>i4'), ('BRICKNAME', 'S8'), ('BRICK_OBJID', '>i4'), ('MORPHTYPE', 'S4'),
+            ('RA', '>f8'), ('RA_IVAR', '>f4'), ('DEC', '>f8'), ('DEC_IVAR', '>f4'), ('DCHISQ', '>f4', (5,)),
+            ('EBV', '>f4'),
+            ('FLUX_G', '>f4'), ('FLUX_R', '>f4'), ('FLUX_Z', '>f4'), ('FLUX_IVAR_G', '>f4'), ('FLUX_IVAR_R', '>f4'),
+            ('FLUX_IVAR_Z', '>f4'), ('MW_TRANSMISSION_G', '>f4'), ('MW_TRANSMISSION_R', '>f4'),
+            ('MW_TRANSMISSION_Z', '>f4'), ('FRACFLUX_G', '>f4'), ('FRACFLUX_R', '>f4'), ('FRACFLUX_Z', '>f4'),
+            ('FRACMASKED_G', '>f4'), ('FRACMASKED_R', '>f4'), ('FRACMASKED_Z', '>f4'), ('FRACIN_G', '>f4'),
+            ('FRACIN_R', '>f4'), ('FRACIN_Z', '>f4'), ('NOBS_G', '>i2'), ('NOBS_R', '>i2'), ('NOBS_Z', '>i2'),
+            ('PSFDEPTH_G', '>f4'), ('PSFDEPTH_R', '>f4'), ('PSFDEPTH_Z', '>f4'), ('GALDEPTH_G', '>f4'),
+            ('GALDEPTH_R', '>f4'), ('GALDEPTH_Z', '>f4'), ('FLUX_W1', '>f4'), ('FLUX_W2', '>f4'), ('FLUX_W3', '>f4'),
+            ('FLUX_W4', '>f4'), ('FLUX_IVAR_W1', '>f4'), ('FLUX_IVAR_W2', '>f4'), ('FLUX_IVAR_W3', '>f4'),
+            ('FLUX_IVAR_W4', '>f4'), ('MW_TRANSMISSION_W1', '>f4'), ('MW_TRANSMISSION_W2', '>f4'),
+            ('MW_TRANSMISSION_W3', '>f4'), ('MW_TRANSMISSION_W4', '>f4'), ('ALLMASK_G', '>i2'), ('ALLMASK_R', '>i2'),
+            ('ALLMASK_Z', '>i2'), ('FIBERFLUX_G', '>f4'), ('FIBERFLUX_R', '>f4'), ('FIBERFLUX_Z', '>f4'),
+            ('FIBERTOTFLUX_G', '>f4'), ('FIBERTOTFLUX_R', '>f4'), ('FIBERTOTFLUX_Z', '>f4'), ('REF_EPOCH', '>f4'),
+            ('WISEMASK_W1', 'u1'), ('WISEMASK_W2', 'u1'), ('MASKBITS', '>i2'), ('LC_FLUX_W1', '>f4', (15,)),
+            ('LC_FLUX_W2', '>f4', (15,)), ('LC_FLUX_IVAR_W1', '>f4', (15,)), ('LC_FLUX_IVAR_W2', '>f4', (15,)),
+            ('LC_NOBS_W1', '>i2', (15,)), ('LC_NOBS_W2', '>i2', (15,)), ('LC_MJD_W1', '>f8', (15,)),
+            ('LC_MJD_W2', '>f8', (15,)), ('SHAPE_R', '>f4'), ('SHAPE_E1', '>f4'), ('SHAPE_E2', '>f4'),
+            ('SHAPE_R_IVAR', '>f4'), ('SHAPE_E1_IVAR', '>f4'), ('SHAPE_E2_IVAR', '>f4'), ('SERSIC', '>f4'),
+            ('SERSIC_IVAR', '>f4'), ('REF_ID', '>i8'), ('REF_CAT', 'S2'), ('GAIA_PHOT_G_MEAN_MAG', '>f4'),
+            ('GAIA_PHOT_G_MEAN_FLUX_OVER_ERROR', '>f4'), ('GAIA_PHOT_BP_MEAN_MAG', '>f4'),
+            ('GAIA_PHOT_BP_MEAN_FLUX_OVER_ERROR', '>f4'), ('GAIA_PHOT_RP_MEAN_MAG', '>f4'),
+            ('GAIA_PHOT_RP_MEAN_FLUX_OVER_ERROR', '>f4'), ('GAIA_PHOT_BP_RP_EXCESS_FACTOR', '>f4'),
+            ('GAIA_ASTROMETRIC_EXCESS_NOISE', '>f4'), ('GAIA_DUPLICATED_SOURCE', '?'),
+            ('GAIA_ASTROMETRIC_SIGMA5D_MAX', '>f4'), ('GAIA_ASTROMETRIC_PARAMS_SOLVED', 'i1'), ('PARALLAX', '>f4'),
+            ('PARALLAX_IVAR', '>f4'), ('PMRA', '>f4'), ('PMRA_IVAR', '>f4'), ('PMDEC', '>f4'), ('PMDEC_IVAR', '>f4'),
+            ('PHOTSYS', '<U1'), ('TARGETID', '>i8'), ('DESI_TARGET', '>i8'), ('BGS_TARGET', '>i8'),
+            ('MWS_TARGET', '>i8'),
+            ('SUBPRIORITY', '>f8'), ('OBSCONDITIONS', '>i8'), ('PRIORITY_INIT_DARK', '>i8'),
+            ('NUMOBS_INIT_DARK', '>i8'),
+            ('PRIORITY_INIT_BRIGHT', '>i8'), ('NUMOBS_INIT_BRIGHT', '>i8'), ('PRIORITY_INIT_BACKUP', '>i8'),
+            ('NUMOBS_INIT_BACKUP', '>i8')]
+    columns = [c[0] for c in cols]
+    df = pd.DataFrame.from_records(data=list(result), columns=columns)
+    cats = df.DESI_TARGET.apply(desi_code_2_cat)
+    df['LRG'], df['ELG'], df['QSO'], df['GLBG'], df['RLBG'] = zip(*cats)
+    return df
 
 
 def apply_cuts_gaia(numproc=4, survey='main', nside=None, pixlist=None,
--- py/desitarget/zz_debugging_driver.py
+++ py/desitarget/zz_debugging_driver.py
@@ -1,9 +1,5 @@
 import os
-
-import numpy as np
-
-from cuts import select_targets
-import pandas as pd
+from cuts import select_targets, format_output
 
 filenames = []
 
@@ -21,92 +17,12 @@ res = select_targets(
     tcnames=["LRG","ELG","QSO", 'LBG'], backup=False)
 
 
-def desitarget_bitcode_2_str(bitcode):
-    bina = (bin(bitcode))
-    bina = bina[2:]
-
-    desi_codes = set()
-
-    i = len(bina) - 1
-    for bit in bina:
-        if bit == '1':
-            desi_codes.add(i)
-        i -= 1
-
-    return desi_codes
-
-
-def desi_code_2_label(code):
-
-    LRG_CODES = {0, 8, 16}
-    ELG_CODES = {1, 5, 6, 7, 9, 11, 12, 17, 19, 20}
-    QSO_CODES = {2, 4, 10, 18}
-    GLBG_CODE = {38}
-    RLBG_CODE = {39}
-
-    lrg = bool(LRG_CODES & code)
-
-    elg = bool(ELG_CODES & code)
-
-    qso = bool(QSO_CODES & code)
-
-    glbg = bool(GLBG_CODE & code)
-
-    rlbg = bool(RLBG_CODE & code)
-
-    return lrg, elg, qso, glbg, rlbg
-
-
-def desi_code_2_cat(bitcode):
-    return desi_code_2_label(desitarget_bitcode_2_str(bitcode))
-
-
-def format_output(result):
-    cols = [('RELEASE', '>i2'), ('BRICKID', '>i4'), ('BRICKNAME', 'S8'), ('BRICK_OBJID', '>i4'), ('MORPHTYPE', 'S4'),
-            ('RA', '>f8'), ('RA_IVAR', '>f4'), ('DEC', '>f8'), ('DEC_IVAR', '>f4'), ('DCHISQ', '>f4', (5,)),
-            ('EBV', '>f4'),
-            ('FLUX_G', '>f4'), ('FLUX_R', '>f4'), ('FLUX_Z', '>f4'), ('FLUX_IVAR_G', '>f4'), ('FLUX_IVAR_R', '>f4'),
-            ('FLUX_IVAR_Z', '>f4'), ('MW_TRANSMISSION_G', '>f4'), ('MW_TRANSMISSION_R', '>f4'),
-            ('MW_TRANSMISSION_Z', '>f4'), ('FRACFLUX_G', '>f4'), ('FRACFLUX_R', '>f4'), ('FRACFLUX_Z', '>f4'),
-            ('FRACMASKED_G', '>f4'), ('FRACMASKED_R', '>f4'), ('FRACMASKED_Z', '>f4'), ('FRACIN_G', '>f4'),
-            ('FRACIN_R', '>f4'), ('FRACIN_Z', '>f4'), ('NOBS_G', '>i2'), ('NOBS_R', '>i2'), ('NOBS_Z', '>i2'),
-            ('PSFDEPTH_G', '>f4'), ('PSFDEPTH_R', '>f4'), ('PSFDEPTH_Z', '>f4'), ('GALDEPTH_G', '>f4'),
-            ('GALDEPTH_R', '>f4'), ('GALDEPTH_Z', '>f4'), ('FLUX_W1', '>f4'), ('FLUX_W2', '>f4'), ('FLUX_W3', '>f4'),
-            ('FLUX_W4', '>f4'), ('FLUX_IVAR_W1', '>f4'), ('FLUX_IVAR_W2', '>f4'), ('FLUX_IVAR_W3', '>f4'),
-            ('FLUX_IVAR_W4', '>f4'), ('MW_TRANSMISSION_W1', '>f4'), ('MW_TRANSMISSION_W2', '>f4'),
-            ('MW_TRANSMISSION_W3', '>f4'), ('MW_TRANSMISSION_W4', '>f4'), ('ALLMASK_G', '>i2'), ('ALLMASK_R', '>i2'),
-            ('ALLMASK_Z', '>i2'), ('FIBERFLUX_G', '>f4'), ('FIBERFLUX_R', '>f4'), ('FIBERFLUX_Z', '>f4'),
-            ('FIBERTOTFLUX_G', '>f4'), ('FIBERTOTFLUX_R', '>f4'), ('FIBERTOTFLUX_Z', '>f4'), ('REF_EPOCH', '>f4'),
-            ('WISEMASK_W1', 'u1'), ('WISEMASK_W2', 'u1'), ('MASKBITS', '>i2'), ('LC_FLUX_W1', '>f4', (15,)),
-            ('LC_FLUX_W2', '>f4', (15,)), ('LC_FLUX_IVAR_W1', '>f4', (15,)), ('LC_FLUX_IVAR_W2', '>f4', (15,)),
-            ('LC_NOBS_W1', '>i2', (15,)), ('LC_NOBS_W2', '>i2', (15,)), ('LC_MJD_W1', '>f8', (15,)),
-            ('LC_MJD_W2', '>f8', (15,)), ('SHAPE_R', '>f4'), ('SHAPE_E1', '>f4'), ('SHAPE_E2', '>f4'),
-            ('SHAPE_R_IVAR', '>f4'), ('SHAPE_E1_IVAR', '>f4'), ('SHAPE_E2_IVAR', '>f4'), ('SERSIC', '>f4'),
-            ('SERSIC_IVAR', '>f4'), ('REF_ID', '>i8'), ('REF_CAT', 'S2'), ('GAIA_PHOT_G_MEAN_MAG', '>f4'),
-            ('GAIA_PHOT_G_MEAN_FLUX_OVER_ERROR', '>f4'), ('GAIA_PHOT_BP_MEAN_MAG', '>f4'),
-            ('GAIA_PHOT_BP_MEAN_FLUX_OVER_ERROR', '>f4'), ('GAIA_PHOT_RP_MEAN_MAG', '>f4'),
-            ('GAIA_PHOT_RP_MEAN_FLUX_OVER_ERROR', '>f4'), ('GAIA_PHOT_BP_RP_EXCESS_FACTOR', '>f4'),
-            ('GAIA_ASTROMETRIC_EXCESS_NOISE', '>f4'), ('GAIA_DUPLICATED_SOURCE', '?'),
-            ('GAIA_ASTROMETRIC_SIGMA5D_MAX', '>f4'), ('GAIA_ASTROMETRIC_PARAMS_SOLVED', 'i1'), ('PARALLAX', '>f4'),
-            ('PARALLAX_IVAR', '>f4'), ('PMRA', '>f4'), ('PMRA_IVAR', '>f4'), ('PMDEC', '>f4'), ('PMDEC_IVAR', '>f4'),
-            ('PHOTSYS', '<U1'), ('TARGETID', '>i8'), ('DESI_TARGET', '>i8'), ('BGS_TARGET', '>i8'),
-            ('MWS_TARGET', '>i8'),
-            ('SUBPRIORITY', '>f8'), ('OBSCONDITIONS', '>i8'), ('PRIORITY_INIT_DARK', '>i8'),
-            ('NUMOBS_INIT_DARK', '>i8'),
-            ('PRIORITY_INIT_BRIGHT', '>i8'), ('NUMOBS_INIT_BRIGHT', '>i8'), ('PRIORITY_INIT_BACKUP', '>i8'),
-            ('NUMOBS_INIT_BACKUP', '>i8')]
-    columns = [c[0] for c in cols]
-    df = pd.DataFrame.from_records(data=list(result), columns=columns)
-    cats = df.DESI_TARGET.apply(desi_code_2_cat)
-    df['LRG'], df['ELG'], df['QSO'], df['GLBG'], df['RLBG'] = zip(*cats)
-    return df
-
 df = format_output(result=res)
 
 print((df.head()))
+print(f"No of LRG       : {len(df[df['LRG'] == True])}")
+print(f"No of ELG       : {len(df[df['ELG'] == True])}")
+print(f"No of QSO       : {len(df[df['QSO'] == True])}")
+print(f"No of G Dropouts: {len(df[df['GLBG'] == True])}")
+print(f"No of R Dropouts: {len(df[df['RLBG'] == True])}")
 
-print("No of LRG       :", len(df[df['LRG'] == True]))
-print("No of ELG       :", len(df[df['ELG'] == True]))
-print("No of QSO       :", len(df[df['QSO'] == True]))
-print("No of G Dropouts:", len(df[df['GLBG'] == True]))
-print("No of R Dropouts:", len(df[df['RLBG'] == True]))
